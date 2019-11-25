35be0e7b469b79fb29588f692d3f1fd03ed21f42
Mike Puskar <puskar49@gmail.com>
2019-11-25 16:38:09 +0000

    Added VarnoType and VertcoType to constants

--- omfg/constants/__init__.py
+++ omfg/constants/__init__.py
@@ -3,12 +3,14 @@
 from .column import Column
 from .subtype import Subtype
 from .varno import Varno
-from .vertco import VertcoType
+from .varno_type import VarnoType
+from .vertco_type import VertcoType
 
 
 __all__ = [
     "Column",
     "Subtype",
     "Varno",
+    "VarnoType",
     "VertcoType"
 ]
