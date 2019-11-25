b299af137ec67dc0677441038d64810b63bcdfbe
Mike Puskar <puskar49@gmail.com>
2019-11-25 23:12:40 +0000

    Added new types

--- omfg/constants/__init__.py
+++ omfg/constants/__init__.py
@@ -1,6 +1,7 @@
 """Module for storing constant variables and static classes."""
 
 from .column import Column
+from .cycle import Cycle
 from .subtype import Subtype
 from .varno import Varno
 from .varno_type import VarnoType
@@ -9,6 +10,7 @@ from .vertco_type import VertcoType
 
 __all__ = [
     "Column",
+    "Cycle",
     "Subtype",
     "Varno",
     "VarnoType",
