3b8a66f403febba46fefb2f1fe25a4cbe72de86c
Mike Puskar <puskar49@gmail.com>
2019-11-25 16:11:55 +0000

    Made column an object

--- omfg/constants/column.py
+++ omfg/constants/column.py
@@ -1,18 +1,38 @@
 """Module for handling column names for label purposes"""
 
-
-class Column:
-    @staticmethod
-    def get_title(column):
-        try:
-            return COLUMN_TITLES[column]
-        except KeyError:
-            return None
-
-
-COLUMN_TITLES = {
+COLUMN_LABELS = {
     "obsvalue@body": None,
     "corvalue@body": "Corrected",
     "an_depar@body": "O-A",
     "fg_depar@body": "O-B"
 }
+
+
+class Column:
+    """A column object"""
+    def __init__(self, name, label):
+        self._name = name
+        self._label = label
+
+    @property
+    def name(self):
+        """The name used by ODB2"""
+        return self._name
+
+    @property
+    def label(self):
+        """The label used by chart generators"""
+        return self._label
+
+    @property
+    def is_depar(self):
+        """True if the column is an_depar or fg_depar"""
+        return "depar" in self.name
+
+    @staticmethod
+    def get_column(name):
+        """Static method for creating Column objects"""
+        try:
+            return Column(name=name, label=COLUMN_LABELS[name])
+        except KeyError:
+            raise ValueError(f"Unknown column name {name}")
